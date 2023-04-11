698a21a2be3b324177105f32a23f23c81d7a02b5
Omar Ignacio Urias Ruiz <omar.ignacio.uri@gmail.com>
2023-04-10 23:36:41 -0700

    Ahora si stable

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const distube = new DisTube(client, {
 	searchSongs: 5,
 	searchCooldown: 30,
 	leaveOnEmpty: true,
-	leaveOnFinish: false,
+	leaveOnFinish: true,
 	leaveOnStop: false,
 });
 
@@ -20,26 +20,33 @@ client.on('ready', client => {
 });
 
 client.on('messageCreate', message => {
-	
+
 	for (const key in responses) {
 		if (message.content.toLowerCase() === key) {
-			message.channel.send(responses[key])
+			message.channel.send(responses[key]);
+			return;
 		}
-		
+
 	}
 	if (message.author.bot || !message.inGuild()) return;
 	const args = message.content.split(' ');
 	if (args[0].toLowerCase === !config.prefix) return;
 
-	const command = args[1].toLowerCase();
+	const command = args[1];
 	args.shift();
 	args.shift();
 
+	console.log(command);
 
+	if (command == undefined) return;
 
-
-
-
+	if (command === 'ayuda') {
+		message.channel.send('Tienes diferentes opciones para comandos solo procura poner "sumi" antes de cada uno\n**play** Es un comando para poner musica en efecto, acepto nombres y links de youtube');
+		message.channel.send('**para** o **stop** Como supiste? para la musica\n**sal** o **leave** En efecto me voy alv \n**pausa** o **pause** Sopresa pauso la musica');
+		message.channel.send('**resume** o **sigue** Sigo con las rolitas \n**skip** Te tengo que decir? \n**lista** te doy la lista de musica actual');
+		message.channel.send('Esto es todo por ahora dudas y sugerencias al creador porfavor, y si llego a morir favor de avisarle al creador asi me debuguea y arregla');
+		return;
+	}
 	if (command === 'pon' || 'play') {
 		distube
 			.play(message.member.voice.channel, args.join(' '), {
@@ -55,42 +62,70 @@ client.on('messageCreate', message => {
 			});
 	}
 	if (['repeat', 'loop'].includes(command)) {
-		const mode = distube.setRepeatMode(message);
-		message.channel.send(
-			`Modo loop \`${mode
-				? mode === 2
-					? 'All Queue'
-					: 'This Song'
-				: 'Off'
-			}\``,
-		);
+		try {
+			const mode = distube.setRepeatMode(message);
+			message.channel.send(
+				`Modo loop \`${mode
+					? mode === 2
+						? 'All Queue'
+						: 'This Song'
+					: 'Off'
+				}\``,
+			);
+		} catch (error) {
+			console.log(error)
+		}
+
 	}
 
 	if (command === 'para' || command === 'stop') {
-		distube.stop(message);
-		message.channel.send('Pare!');
+		try {
+			distube.stop(message);
+			message.channel.send('Pare!');
+		} catch (error) {
+			console.log(error);
+		}
+
 	}
 
 	if (command === 'sal' || command === 'leave') {
 		distube.voices.get(message)?.leave();
 		message.channel.send('Ya me voy pues');
 	}
-	if (command === 'resume' || command === 'sigue') distube.resume(message);
+	if (command === 'resume' || command === 'sigue') {
+		try {
+			distube.resume(message);
+		} catch (error) {
+			console.log(error);
+		}
 
-	if (command === 'pause' || command === 'pausa' || command === 'espera') distube.pause(message);
+	}
 
-	if (command === 'skip' || command === 'siguente') {
-		const queue = distube.getQueue(message);
-		console.log(queue.songs.length);
-		if (queue.songs.length == 1) {
-			distube.voices.get(message)?.leave();
-
-		} else {
-			distube.skip(message);
+	if (command === 'pause' || command === 'pausa' || command === 'espera') {
+		try {
+			distube.pause(message);
+		} catch (error) {
+			console.log(error)
 		}
 	}
 
-	if (command === 'queue'|| command === 'lista') {
+	if (command === 'skip' || command === 'siguente') {
+		try {
+			const queue = distube.getQueue(message);
+			console.log(queue.songs.length);
+			if (queue.songs.length == 1) {
+				distube.voices.get(message)?.leave();
+
+			} else {
+				distube.skip(message);
+			}
+		} catch (error) {
+			console.log(error)
+		}
+
+	}
+
+	if (command === 'queue' || command === 'lista') {
 		const queue = distube.getQueue(message);
 		if (!queue) {
 			message.channel.send('No hay nada wey');
